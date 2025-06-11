a51bfba5617beabd2cf5cce3f61a3bc5742c59d1
SumanrajChandrasekar <168365780+SumanrajChandrasekar@users.noreply.github.com>
2025-06-11 18:23:06 +0530

    Delete DemoProject directory

--- DemoProject/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsernameAndPassword(String username, String password); // Remove Role
-    User findByUsername(String username); // Add this to find user by username
-}
