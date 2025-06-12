391fca40e6fb4588b36578c93c419c151bc18ec2
SumanrajChandrasekar <168365780+SumanrajChandrasekar@users.noreply.github.com>
2025-06-12 12:10:57 +0530

    to live api Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: true,
+//   authApiUrl: 'https://9000.com/api/auth',
+//   studentApiUrl: 'https://9000.com/api/student'
+// };
+
 export const environment = {
   production: true,
-  authApiUrl: 'https://9000.com/api/auth',
-  studentApiUrl: 'https://9000.com/api/student'
+  authApiUrl: 'https://student-management-with-security.onrender.com/api/auth',
+  studentApiUrl: 'https://student-management-with-security.onrender.com/api/student'
 };
